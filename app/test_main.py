c88784dd5dbfb09a610e9ca5bab3d8ba28b41dbd
Reverad <sektumpro@gmail.com>
2026-03-12 16:17:03 +0200

    flake8 fix

--- app/test_main.py
+++ app/test_main.py
@@ -3,24 +3,24 @@ from app.main import check_password
 
 
 def test_should_check_min_length() -> None:
-    assert check_password("Ab@12") == False, "too short"
+    assert check_password("Ab@12") is False, "too short"
 
 
 def test_should_check_max_length() -> None:
-    assert check_password("A" * 17 + "@1") == False, "too long"
+    assert check_password("A" * 17 + "@1") is False, "too long"
 
 
 def test_should_check_digit() -> None:
-    assert check_password("Password@") == False, "no digits"
+    assert check_password("Password@") is False, "no digits"
 
 
 def test_should_check_special_symbols() -> None:
-    assert check_password("Password1") == False, "no special symbols"
+    assert check_password("Password1") is False, "no special symbols"
 
 
 def test_should_check_upper_letter() -> None:
-    assert check_password("password1@") == False, "no upper letters"
+    assert check_password("password1@") is False, "no upper letters"
 
 
 def test_should_check_valid_value() -> None:
-    assert check_password("Pass@word1") == True, "password is valid"
+    assert check_password("Pass@word1") is True, "password is valid"
